03312b4d4a83df1d527b9ac4777923eada30a18f
tinker1234 <30789433+tinker1234@users.noreply.github.com>
2017-09-04 00:43:42 -0400

    Update header.py

--- header.py
+++ header.py
@@ -1,1 +1,1 @@
-header = "Transfer-Encoding: chuncked\r\nContent-Type: audio/mp3\r\nPyc-Title: $title\r\nPyc-Length: $length\r\nPyc-Listeners: $listen\r\nPyc-Max-Listeners: $max\r\n\r\n"
+header = "Transfer-Encoding: chuncked\r\nContent-Type: audio/mp3\r\nPyc-Title: $title\r\nPyc-Length: $length\r\nPyc-Bitrate: $bitrate\r\nPyc-Listeners: $listen\r\nPyc-Max-Listeners: $max\r\n\r\n"
