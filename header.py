68001747996af210018d2a050e2034114accb466
tinker1234 <30789433+tinker1234@users.noreply.github.com>
2018-03-13 11:10:30 -0400

    Update header.py

--- header.py
+++ header.py
@@ -1,1 +1,2 @@
 header = "Transfer-Encoding: chuncked\r\nContent-Type: audio/mp3\r\nPyc-Title: $title\r\nPyc-Length: $length\r\nPyc-Bitrate: $bitrate\r\nPyc-Listeners: $listen\r\nPyc-Max-Listeners: $max\r\n\r\n"
+HTTP_RESP = "Content-Type: {0}\r\nContent-Length: {1}\r\n\r\n{2}"
